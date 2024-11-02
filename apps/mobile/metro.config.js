12daa1b5e864d34e839cdc11e03c2aadc4e6cfb3
brianMxBm <brian.melgar.mx@gmail.com>
2024-11-01 19:42:06 -0700

    [FE]: Init SVG Support

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,26 +1,37 @@
-// Learn more https://docs.expo.dev/guides/monorepos
 const { getDefaultConfig } = require('expo/metro-config');
 const { FileStore } = require('metro-cache');
 const { withNativeWind } = require('nativewind/metro');
 const path = require('path');
 
+// Find the project and workspace directories
 const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, '../..');
+const monorepoRoot = path.resolve(projectRoot, '../..');
 
 const config = getDefaultConfig(projectRoot);
 
-// Since we are using pnpm, we have to setup the monorepo manually for Metro
-// #1 - Watch all files in the monorepo
-config.watchFolders = [workspaceRoot];
-// #2 - Try resolving with project modules first, then workspace modules
+// Set up watch folders for monorepo
+config.watchFolders = [monorepoRoot];
+
+// SVG transformer configuration
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+};
+
+// Set up module resolution for node_modules
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
-  path.resolve(workspaceRoot, 'node_modules'),
+  path.resolve(monorepoRoot, 'node_modules'),
 ];
 
-// Use turborepo to restore the cache when possible
+// Modify assetExts and sourceExts for SVG support
+config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== 'svg');
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
+
+// Use TurboRepo to restore the cache when possible
 config.cacheStores = [
   new FileStore({ root: path.join(projectRoot, 'node_modules', '.cache', 'metro') }),
 ];
 
+// Enable NativeWind with global CSS input
 module.exports = withNativeWind(config, { input: './global.css' });
