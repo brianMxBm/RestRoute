1c1d4c8b471544609e1785f6292f81ec9b8bf8fb
brianMxBm <brian.mx.melgar@gmail.com>
2025-01-20 15:46:01 -0800

    [GE]: New Font Asset + Setup Expo Fonts

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -2,6 +2,7 @@ import '../../global.css';
 import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useFonts } from 'expo-font';
 import { Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
@@ -12,6 +13,28 @@ import { SessionProvider } from '../providers/Auth/SessionProvider';
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Raleway: require('../../assets/fonts/Raleway-Regular.ttf'),
+    RalewayBold: require('../../assets/fonts/Raleway-Bold.ttf'),
+    RalewayItalic: require('../../assets/fonts/Raleway-Italic.ttf'),
+    RalewayBoldItalic: require('../../assets/fonts/Raleway-BoldItalic.ttf'),
+    RalewayExtraBold: require('../../assets/fonts/Raleway-ExtraBold.ttf'),
+    RalewayExtraBoldItalic: require('../../assets/fonts/Raleway-ExtraBoldItalic.ttf'),
+    RalewayMedium: require('../../assets/fonts/Raleway-Medium.ttf'),
+    RalewayMediumItalic: require('../../assets/fonts/Raleway-MediumItalic.ttf'),
+    RalewayLight: require('../../assets/fonts/Raleway-Light.ttf'),
+    RalewayLightItalic: require('../../assets/fonts/Raleway-LightItalic.ttf'),
+    RalewayRegular: require('../../assets/fonts/Raleway-Regular.ttf'),
+    RalewaySemiBold: require('../../assets/fonts/Raleway-SemiBold.ttf'),
+    RalewaySemiBoldItalic: require('../../assets/fonts/Raleway-SemiBoldItalic.ttf'),
+    RalewayThin: require('../../assets/fonts/Raleway-Thin.ttf'),
+    RalewayThinItalic: require('../../assets/fonts/Raleway-ThinItalic.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return;
+  }
+
   if (!publishableKey) {
     throw new Error(
       'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env'
@@ -28,6 +51,7 @@ export default function RootLayout() {
             <StatusBar style="dark" />
             <BottomSheetModalProvider>
               <SessionProvider>
+                <StatusBar style="light" />
                 <Slot />
               </SessionProvider>
             </BottomSheetModalProvider>
--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -8,6 +8,23 @@ module.exports = {
   presets: [require('nativewind/preset')],
   theme: {
     colors,
+    fontFamily: {
+      raleway: ['Raleway', 'sans-serif'],
+      ralewayItalic: ['RalewayItalic', 'sans-serif'],
+      ralewayBold: ['RalewayBold', 'sans-serif'],
+      ralewayBoldItalic: ['RalewayBoldItalic', 'sans-serif'],
+      ralewayExtraBold: ['RalewayExtraBold', 'sans-serif'],
+      ralewayExtraBoldItalic: ['RalewayExtraBoldItalic', 'sans-serif'],
+      ralewayMedium: ['RalewayMedium', 'sans-serif'],
+      ralewayMediumItalic: ['RalewayMediumItalic', 'sans-serif'],
+      ralewayLight: ['RalewayLight', 'sans-serif'],
+      ralewayLightItalic: ['RalewayLightItalic', 'sans-serif'],
+      ralewayRegular: ['RalewayRegular', 'sans-serif'],
+      ralewaySemiBold: ['RalewaySemiBold', 'sans-serif'],
+      ralewaySemiBoldItalic: ['RalewaySemiBoldItalic', 'sans-serif'],
+      ralewayThin: ['RalewayThin', 'sans-serif'],
+      ralewayThinItalic: ['RalewayThinItalic', 'sans-serif'],
+    },
     extend: {
       borderWidth: {
         1: '1px', // Adding a 1px border width
